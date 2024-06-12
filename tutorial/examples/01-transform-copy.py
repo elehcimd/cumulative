acfbc10d952e73ee6ccf6b030053fde4621f06f1
Michele Dallachiesa <michele.dallachiesa@sigforge.com>
2024-06-12 09:24:18 +0200

    Deployed 26f99fe with MkDocs version: 1.6.0

---
+++ tutorial/examples/01-transform-copy.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from cumulative import Cumulative
+
+df = pd.DataFrame(
+    {
+        "base.x": [[0, 1, 2, 3, 4, 5], [0, 1, 2, 3]],
+        "base.y": [[10, 20, 30, 40, 50, 60], [5, 15, 25, 35]],
+    }
+)
+
+c = Cumulative(df)
+c.copy(src="base", dst="test")
+
+print("Type: ", type(c.df))
+print("--")
+print(c.df)
---
+++ tutorial/examples/01-transform-pipe.py.src
@@ -0,0 +1,14 @@
+import pandas as pd
+from cumulative import Cumulative
+
+df = pd.DataFrame(
+    {
+        "base.x": [[0, 1, 2, 3, 4, 5], [0, 1, 2, 3]],
+        "base.y": [[10, 20, 30, 40, 50, 60], [5, 15, 25, 35]],
+    }
+)
+
+c = Cumulative(df)
+c.cumsum(src="base", dst="C").scale(src="C", dst="S", kind="xy")
+
+print(c.df.iloc[0])
