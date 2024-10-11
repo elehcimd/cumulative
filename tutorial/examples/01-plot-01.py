c436c0f4c7c4ca9da4d7cdc8c1d5b22cbfcc14cc
Michele Dallachiesa <michele.dallachiesa@sigforge.com>
2024-10-11 12:17:17 +0200

    Deployed bdf19e6 with MkDocs version: 1.6.1

--- tutorial/examples/01-plot-01.py
+++ tutorial/examples/01-plot-01.py
@@ -1,10 +1,12 @@
 from scipy.stats import cosine
 
 from cumulative.datasets.load_dist import load_dist
+from cumulative.plotting import plot_ctx
 
 c = load_dist([cosine] * 100, kind="rvs")
 
 print("Dataset overview:")
 c.describe()
 
-c.plot.scatter(style="-", color="blue", alpha=0.1).render()
+with plot_ctx() as ax:
+    c.plot.draw(ax=ax, style="-", color="blue", alpha=0.1)
